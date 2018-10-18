1f005819a3238c0653a0e83623e479079175ce10
AashutoshSharma <44224506+AashutoshSharma@users.noreply.github.com>
2018-10-18 11:37:45 +0530

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/*** Child Theme Function  ***/
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function woly_elated_child_theme_enqueue_scripts() {
+	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+	wp_enqueue_style( 'childstyle' );
+}
+add_action('wp_enqueue_scripts', 'woly_elated_child_theme_enqueue_scripts', 11);
+add_filter( 'woocommerce_subcategory_count_html', '__return_null' );
